d9bab4e8467da4112845ce650079b7f09d4a96a9
Ryan Pramasyana <ryan.pramasyana@gotoko.co.id>
2026-07-16 19:40:03 +0700

    "fix: prefix financing route names to avoid collision with service routes

--- routes/api/v1/tenant/financing.php
+++ routes/api/v1/tenant/financing.php
@@ -12,7 +12,7 @@ Route::get('/financing/monthly', [CostController::class, 'monthly']);
 Route::get('/financing/breakdown', [CostController::class, 'breakdown']);
 Route::post('/financing/import', [CostController::class, 'import']);
 
-Route::apiResource('/financing/costs', CostController::class)->except(['show']);
+Route::apiResource('/financing/costs', CostController::class)->except(['show'])->names('financing.costs');
 
-Route::apiResource('/financing/categories', CostCategoryController::class)->except(['show', 'index']);
+Route::apiResource('/financing/categories', CostCategoryController::class)->except(['show', 'index'])->names('financing.categories');
 Route::get('/financing/categories', [CostCategoryController::class, 'index']);
